9d4f6a736c5e22d3c2d45a6ea04b16b27f0d02eb
MTBakalov18 <MTBakalov18@codingburgas.bg>
2022-11-14 13:56:46 +0200

    Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Users(ID, username, password, FirstName, LastName, role, team)
+VALUES (1, 'MTBakalov18','goshko123', 'Martin', 'Bakalov', 'CEO', 'team1');
+
+INSERT INTO Teams(TeamID, TeamName, CurrentProject, Developers, TeamLead)
+VALUES (1, 'team1', 'project1', 'MTBakalov18', 'MTBakalov18');
+
+INSERT INTO Project(ProjectID, ProjectName, Description, CurrentTeam)
+VALUES (1, 'project1', 'doessomething', 'team1');
+
+INSERT INTO Dismission(DismissionID, DateFrom, DateTo, DateofDismissionCreation, HalfaDayDismission, Approved, Applicant)
+VALUES (1, '11/14', '11/20', '11/10', 'false', 'true', 'Petio');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Users (
+                       ID INTEGER NOT NULL,
+                       username NVARCHAR(50) NOT NULL,
+                       password NVARCHAR(50) NOT NULL,
+                       FirstName NVARCHAR(50) NOT NULL,
+                       LastName NVARCHAR(50) NOT NULL,
+                       role NVARCHAR(50) NOT NULL,
+                       team NVARCHAR(50) NOT NULL,
+                       PRIMARY KEY (ID)
+);
+CREATE TABLE Teams (
+                       TeamID INTEGER NOT NULL,
+                       TeamName NVARCHAR(50) NOT NULL,
+                       CurrentProject NVARCHAR(50) NOT NULL,
+                       Developers NVARCHAR(50) NOT NULL,
+                       TeamLead NVARCHAR(50) NOT NULL,
+                       PRIMARY KEY (TeamID)
+);
+CREATE TABLE Project (
+                         ProjectID INTEGER NOT NULL,
+                         ProjectName NVARCHAR(50) NOT NULL,
+                         Description NVARCHAR(50) NOT NULL,
+                         CurrentTeam NVARCHAR(50) NOT NULL,
+                         PRIMARY KEY (ProjectID)
+);
+CREATE TABLE Dismission (
+                            DismissionID INTEGER NOT NULL,
+                            DateFrom NVARCHAR(50) NOT NULL,
+                            DateTo NVARCHAR(50) NOT NULL,
+                            DateofDismissionCreation NVARCHAR(50) NOT NULL,
+                            HalfaDayDismission BOOLEAN NOT NULL,
+                            Approved BOOLEAN NOT NULL,
+                            Applicant NVARCHAR(50) NOT NULL,
+                            PRIMARY KEY (DismissionID)
+
+)
